4aeb9a9a7f836214dd4a07aadfac2b3ce3f205c6
Moyassine1 <yassineaddi2001@gmail.com>
2023-10-05 15:52:19 -0700

    canelo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,12 @@
-#!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -o quote 101-quote.c
+#include <stdio.h>
+#include <unistd.h>
 
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
+int main(void)
+{
+    write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
+}
