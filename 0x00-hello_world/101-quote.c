70da4fc17c193a91a095c43798ee61239e5fa05a
Moyassine1 <yassineaddi2001@gmail.com>
2023-10-05 15:44:28 -0700

    coding is life

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,3 @@
-#include <unistd.h>
-
-int main(void)
-{
-    const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = 59;  // Length of the quote excluding the null terminator
-
-    // Write the quote to standard error (file descriptor 2)
-    write(2, quote, len);
-
-    return 1;
-}
+#!/bin/bash
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -o quote 101-quote.c
 
